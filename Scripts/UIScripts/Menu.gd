03f29a0e3dff7009152449f4f988be4aeb7e1458
stvnchng <sc111@rice.edu>
2022-02-06 13:23:31 -0600

    add main menu for joe WARREN

---
+++ Scripts/UIScripts/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/Main.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_Join_pressed():
+	pass # Replace with function body.
+
