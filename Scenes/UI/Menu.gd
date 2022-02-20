09101faa3e12ab146fc32b6d94ca7227f6d6074c
stvnchng <sc111@rice.edu>
2022-02-19 23:49:18 -0600

    add new button to menu

--- Scenes/UI/Menu.gd
+++ Scenes/UI/Menu.gd
@@ -5,11 +5,18 @@ onready var start_btn = $MarginContainer/VBoxContainer/Buttons/MarginContainer/S
 func _ready():
 	start_btn.grab_focus()
 
-func _on_Start_pressed():
+
+func _on_Play_pressed():
 	get_tree().change_scene("res://Scenes/Main.tscn")
+
+
+func _on_Host_pressed():
+	get_tree().change_scene("res://Scenes/Main.tscn")
+
 
 func _on_Join_pressed():
 	pass # Replace with function body.
+
 
 func _on_Quit_pressed():
 	get_tree().quit()
