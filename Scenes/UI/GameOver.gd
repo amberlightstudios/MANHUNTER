d4c3e40faa363a7d36de8cd3447719150e8b1148
stvnchng <sc111@rice.edu>
2022-02-19 23:08:04 -0600

    add keyboard control for game over screen

--- Scenes/UI/GameOver.gd
+++ Scenes/UI/GameOver.gd
@@ -1,8 +1,15 @@
 extends Control
 
+onready var restart = $"MarginContainer/VBoxContainer/Buttons/MarginContainer/Restart"
 
+func _ready():
+	restart.grab_focus()
+	
 func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/Main.tscn")
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://Scenes/UI/Menu.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
