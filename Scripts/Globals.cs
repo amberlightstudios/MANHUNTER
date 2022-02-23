3114396ea6f6b065902ca66fb6ee75c28fca774b
stvnchng <sc111@rice.edu>
2022-02-22 22:17:34 -0600

    bunch of ui updates

--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -2,6 +2,7 @@ public static class Globals
 {
 	public static bool SinglePlayer = true;
 	public static bool IsHost = false;
+	public static string PlayerName = "";
 	public static string HostAddress = "";	
-	public static string LastPlayedLevel = "";
+	public static int LastPlayedLevel = 1;
 }
