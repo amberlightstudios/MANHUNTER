9529306a322dc74c1bff48d99df8590d08eecf39
stvnchng <sc111@rice.edu>
2022-02-25 21:30:10 -0600

    fix invalid anim references

--- Scripts/GoblinStates/LadderClimbState.cs
+++ Scripts/GoblinStates/LadderClimbState.cs
@@ -2,50 +2,50 @@ using Godot;
 
 namespace GoblinStates
 {
-    public class LadderClimbState : GoblinState 
-    {
-        public LadderClimbState(Goblin player)
-        {
-            this.player = player;
-            player.SetZeroGravity();
-            player.SetLadderCollision(false);
-            player.PlayAnimation("Ladder");
-        }
+	public class LadderClimbState : GoblinState 
+	{
+		public LadderClimbState(Goblin player)
+		{
+			this.player = player;
+			player.SetZeroGravity();
+			player.SetLadderCollision(false);
+			player.PlayAnimation("Walk");
+		}
 
-        public override void _Process(float delta)
-        {   
-            player.PlayAnimation("Ladder");
-            if (Input.IsActionPressed("move_up")) {
-                player.Velocity = new Vector2(0, -player.LadderClimbSpeed);
-            } else if (Input.IsActionPressed("move_down")) {
-                player.Velocity = new Vector2(0, player.LadderClimbSpeed);
-            } else {
-                player.Velocity = Vector2.Zero;
-                player.PlayAnimation("Idle");
-            }
+		public override void _Process(float delta)
+		{   
+			player.PlayAnimation("Walk");
+			if (Input.IsActionPressed("move_up")) {
+				player.Velocity = new Vector2(0, -player.LadderClimbSpeed);
+			} else if (Input.IsActionPressed("move_down")) {
+				player.Velocity = new Vector2(0, player.LadderClimbSpeed);
+			} else {
+				player.Velocity = Vector2.Zero;
+				player.PlayAnimation("Idle");
+			}
 
-            if (Input.IsActionPressed("move_left") || Input.IsActionPressed("move_right")) {
-                ExitState(new JumpState(player, true));
-                return;
-            }
-        }
+			if (Input.IsActionPressed("move_left") || Input.IsActionPressed("move_right")) {
+				ExitState(new JumpState(player, true));
+				return;
+			}
+		}
 
-        public override void _PhysicsProcess(float delta)
-        {
-            if (!player.OnLadder()) {
-                if (Input.IsActionPressed("move_up")) {
-                    ExitState(new JumpState(player, false));
-                } else if (Input.IsActionPressed("move_down")) {
-                    ExitState(new JumpState(player, true));
-                }
-                return;
-            }
-        }
+		public override void _PhysicsProcess(float delta)
+		{
+			if (!player.OnLadder()) {
+				if (Input.IsActionPressed("move_up")) {
+					ExitState(new JumpState(player, false));
+				} else if (Input.IsActionPressed("move_down")) {
+					ExitState(new JumpState(player, true));
+				}
+				return;
+			}
+		}
 
-        public override void ExitState(GoblinState newState)
-        {
-            player.ReturnNormalGravity();
-            base.ExitState(newState);
-        }
-    }
+		public override void ExitState(GoblinState newState)
+		{
+			player.ReturnNormalGravity();
+			base.ExitState(newState);
+		}
+	}
 }
