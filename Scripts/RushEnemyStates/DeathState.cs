7e251d1bca2660afd229f3e9c736c2feea2423c6
ooyjian <ouyujian14@gmail.com>
2022-03-03 21:17:57 -0600

    fix enemies not dying in networking bug

--- Scripts/RushEnemyStates/DeathState.cs
+++ Scripts/RushEnemyStates/DeathState.cs
@@ -4,16 +4,25 @@ namespace RushEnemyStates
 {
 	public class DeathState : RushEnemyState
 	{
+		bool isPlayingAnim = false;
+
 		public DeathState(RushingEnemy enemy) 
 		{
+			enemy.GetParent().RemoveChild(enemy); // Remove this later
 			this.enemy = enemy;
 			enemy.Velocity = new Vector2(0, enemy.Velocity.y);
-			enemy.PlayAnimation("Death");
 		}
 
 		public override void _Process(float delta)
 		{
+			if (!isPlayingAnim) {
+				// enemy.PlayAnimation("Death");
+				isPlayingAnim = true;
+			}
 
+			if (isPlayingAnim && !enemy.AnimPlayer.IsPlaying()) {
+				enemy.GetParent().RemoveChild(enemy);
+			}
 		}
 
 		public override void _PhysicsProcess(float delta)
--- Scripts/ShooterStates/DeathState.cs
+++ Scripts/ShooterStates/DeathState.cs
@@ -4,16 +4,25 @@ namespace ShooterStates
 {
 	public class DeathState : ShooterState
 	{
+		bool isPlayingAnim = false;
+
 		public DeathState(StaticShooter shooter) 
 		{
+			shooter.GetParent().RemoveChild(shooter); // Remove this later
 			this.shooter = shooter;
 			shooter.Velocity = new Vector2(0, shooter.Velocity.y);
-			shooter.PlayAnimation("Death");
 		}
 
 		public override void _Process(float delta)
 		{
+			if (shooter.OnGround() && !isPlayingAnim) {
+				// shooter.PlayAnimation("Death");
+				isPlayingAnim = true;
+			}
 
+			if (isPlayingAnim && !shooter.AnimPlayer.IsPlaying()) {
+				shooter.GetParent().RemoveChild(shooter);
+			}
 		}
 
 		public override void _PhysicsProcess(float delta)
