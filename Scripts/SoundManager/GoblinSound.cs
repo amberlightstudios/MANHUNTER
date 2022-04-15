724eca9836f075601d2f35cfc2d4a164f2ef3f60
stvnchng <sc111@rice.edu>
2022-04-14 23:24:08 -0500

    add new swipe sound

--- Scripts/SoundManager/GoblinSound.cs
+++ Scripts/SoundManager/GoblinSound.cs
@@ -7,32 +7,37 @@ public class GoblinSound : AudioStreamPlayer2D
 	// private int a = 2;
 	// private string b = "text";
 	private AudioStream StreamAttack;
+	private AudioStream StreamAttackHit;
 	private AudioStream StreamJump;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		StreamAttack = (AudioStream) ResourceLoader.Load("res://Sounds/Melee.wav");
+		StreamAttack = (AudioStream) ResourceLoader.Load("res://Sounds/Swipe.wav");
+		StreamAttackHit = (AudioStream) ResourceLoader.Load("res://Sounds/Melee.wav");
 		StreamJump = (AudioStream) ResourceLoader.Load("res://Sounds/Jump.wav");
 	}
 
 	public void PlaySound(string name) 
 	{
-		switch (name) {
-			case "BasicIdle":
-			case "BasicIdle2":
-			case "BasicMoving":
-			case "BasicMoving2":
-				Stream = StreamAttack;
-				Play();
-				break;
-			case "Jump":
+		switch (name) 
+		{
+		case "BasicIdle":
+		case "BasicIdle2":
+		case "BasicMoving":
+		case "BasicMoving2":
+			Stream = StreamAttack;
+			break;
+		case "AttackHit":
+			Stream = StreamAttackHit;
+			break;
+		case "Jump":
 //				Stream = StreamJump;
-//				Play();
 //				break;
-			default:
+		default:
 //				GD.Print(name + " does not have audio associated with it!");
-				break;
+			break;
 		}
+		Play();
 	}
 }
